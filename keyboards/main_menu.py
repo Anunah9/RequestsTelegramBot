cde948ee09c322c2eba64f30480a2666446c1cbe
Aleksandr <sanek00000007@gmail.com>
2024-12-16 10:43:03 +0300

    Added keyboard for main menu

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -2,14 +2,16 @@ from aiogram.types import (
     ReplyKeyboardMarkup,
     KeyboardButton,
 )
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from assets.rights import RIGHTS_ASSERTIONS
 
 
-def main_menu_keybord() -> ReplyKeyboardMarkup:
+def main_menu_keybord(rights: set) -> ReplyKeyboardMarkup:
     """Клавиатура основного меню для пользователя"""
-    kb = [
-        [KeyboardButton(text="Регистрация")],
-    ]
-    keyboard = ReplyKeyboardMarkup(
-        keyboard=kb, resize_keyboard=True, one_time_keyboard=True
-    )
-    return keyboard
+
+    builder = ReplyKeyboardBuilder()
+    print(rights)
+    for btn in rights["rights"]:
+        builder.add(KeyboardButton(text=RIGHTS_ASSERTIONS[btn]))
+    builder.adjust(2)
+    return builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
