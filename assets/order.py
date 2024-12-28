5b1d8887534a4efc3fbd31ce3dc608b8d7659d00
Aleksandr <sanek00000007@gmail.com>
2024-12-28 16:00:50 +0300

    -Added class for order states -Edited order class

--- assets/order.py
+++ assets/order.py
@@ -1,17 +1,28 @@
-from typing import Union
-
-
+from typing import List, Union
+from aiogram.fsm.state import StatesGroup, State
+from assets.user import User
 STATUSES = {
-    1: "Created",
-    2: "Sended",
-    3: "Accepted",
-    4: "In work",
-    5: "Completed"
+    1: "Создана",
+    2: "Отправлена",
+    3: "Принята",
+    4: "В работе",
+    5: "Закрыта"
 }
 
 
+class OrderStates(StatesGroup):
+    # Состояния для работы с заявками
+    set_text = State()
+    set_departments = State()
+    set_status = State()
+    set_workers = State()
+
+
 class Order:
-    def __init__(self, text, department=None, status:int = 1):
+    def __init__(self, text, status: str):
         self.text: str = text
-        self.department: Union[str, list] = department
+        self.department: Union[str, list]
         self.status: str = status
+        self.workers: Union[User|List]
+
+
